0b1d973e52b68482681225bcb774c21587d4456e
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-07-19 16:35:11 +0200

    fx

--- routes/booking.js
+++ routes/booking.js
@@ -6,12 +6,14 @@ const {
   updateBooking,
   addBooking,
   deleteBooking,
+  getMine,
 } = require("../controllers/booking");
 
 const auth = require("../middleware/auth");
 const protectRoute = require("../middleware/protectRoutes");
 
 router.get("/", auth, getAll);
+router.get("/:id", auth, protectRoute(["suppliers"]), getMine);
 router.post("/", auth, addBooking);
 router.put("/", auth, updateBooking);
 router.delete("/:id", auth, deleteBooking);
